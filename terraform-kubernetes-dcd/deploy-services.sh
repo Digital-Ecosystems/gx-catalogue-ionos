64e1eac2abc3c7f176d4eb3eeaa66896c376678b
Angel Kafazov <akafazov@daiteap.com>
2023-04-27 18:59:01 +0300

    Fix instructions

--- terraform-kubernetes-dcd/deploy-services.sh
+++ terraform-kubernetes-dcd/deploy-services.sh
@@ -5,6 +5,10 @@ if [ -z `printenv TF_VAR_domain` ]; then
     exit 1
 fi  
 
+if [ -z `printenv TF_VAR_kubeconfig` ]; then
+    echo "Stopping because TF_VAR_kubeconfig is undefined"
+    exit 1
+fi  
 
 # This script is used to build the cloud landscape for the federated catalogue.
 terraform init && terraform refresh && terraform plan && terraform apply -auto-approve
